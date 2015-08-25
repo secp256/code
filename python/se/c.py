61912e11e65ab3d216175226f9175cbd9183aeed
zhuliting <zhuliting1986@gmail.com>
2015-08-25 12:45:43 +0800

    updated

--- python/se/c.py
+++ python/se/c.py
@@ -3,6 +3,9 @@
 import searchengine
 
 pages = ['https://www.python.org/']
-crawler = searchengine.crawler('seindex.db')
-#crawler.createindextables()
+crawler = searchengine.crawler('index.db')
+crawler.createindextables()
 crawler.crawl(pages)
+crawler.calculate_page_rank()
+#cur=crawler.con.execute('select * from pagerank order by score desc')
+#for i in range(3): print cur.next( )
--- python/se/s.py
+++ python/se/s.py
@@ -2,7 +2,7 @@
 
 import searchengine
 
-e = searchengine.searcher('seindex.db')
+e = searchengine.searcher('index.db')
 #print e.get_all_words()
 #print e.get_matchrows('python google')
 e.query('python google')
