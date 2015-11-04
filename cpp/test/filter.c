c61d21524a81e6d4c63d1ca80810e2153e4a1a4f
albert <zhuliting1986@gmail.com>
2015-11-04 21:15:59 +0800

    Create filter.c

---
+++ cpp/test/filter.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAXLINE 1000
+
+int main()
+{
+    char line[MAXLINE];
+    FILE *fpin;
+
+    if ((fpin = popen("./myuclc", "r")) == NULL) {
+        printf("popen error");
+        return -1;
+    }
+
+    for (; ;) {
+        fputs("prompt> ", stdout);
+        fflush(stdout);
+        if (fgets(line, MAXLINE, fpin) == NULL) {
+            break;
+        }
+        if (fputs(line, stdout) == EOF) {
+            printf("fputs error to pipi");
+            return -1;
+        }
+    }
+
+    if (pclose(fpin) == -1) {
+        printf("pclose error");
+        return -1;
+    }
+    putchar('\n');
+    return 0;
+}
