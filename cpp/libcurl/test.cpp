f2cca69d2b41f07bc735162ffbefc4ad7f61434f
zhuliting <zhuliting1986@gmail.com>
2015-11-25 17:09:39 +0800

    updated

--- cpp/libcurl/test.cpp
+++ cpp/libcurl/test.cpp
@@ -17,16 +17,16 @@ int main()
 
     if (0 != rc) {
         cout << "curl perform failed, code: " << rc << endl;
-        // return -1;
+        return -1;
     }
-    // cout << "response header: " << ccurl.get_head() << endl;
+    cout << "response header: " << ccurl.get_head() << endl;
 
     // get download info
     cout << "status code: " << ccurl.get_curl_status_code() << endl;
 
-    // char *p = NULL;
-    // ccurl.get_last_url(p);
-    // cout << "last url: " << p << endl;
+    char *p = NULL;
+    ccurl.get_last_url(p);
+    cout << "last url: " << p << endl;
 
     curl_global_cleanup();
     return 0;
