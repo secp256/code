0753a430743080fc716214bd469db2f1b1716275
zhuliting <zhuliting1986@gmail.com>
2015-11-26 10:52:21 +0800

    updated

--- lib/libcurl/ccurl.h
+++ lib/libcurl/ccurl.h
@@ -15,17 +15,29 @@ private:
     void set_curl_opt();
 
 public:
-    // set
+    // set download url
     void set_url(const char *url);
 
-    // get
+    // add request http head
+    void add_request_head(const std::string &key, const std::string &value);
+    // clear request http head
+    void clear_request_head();
+
+    // get response body
     std::string get_body() { return resp_body_; }
+    // get response head
     std::string get_head() { return resp_head_; }
 
+    // perfrom curl
     int download();
 
+    // get download info
+    // get status code
     int get_curl_status_code();
-    int get_last_url(char *p);
+    // get redirected url
+    int get_last_url(char **p);
+    // get content-type
+    int get_content_type(char **p);
 
 private:
     size_t write_head(void *ptr, size_t size, size_t nmemb);
@@ -40,6 +52,8 @@ private:
     std::string url_;
     std::string resp_body_;
     std::string resp_head_;
+
+	struct curl_slist *head_list_;
 };
 
 #endif // INCLUDE_CURL_H
