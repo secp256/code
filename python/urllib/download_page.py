8a89ec2f1c6cfb9d10bd83eb50745e289bf359eb
zhuliting <zhuliting1986@gmail.com>
2015-11-27 10:00:01 +0800

    download_page.py added

--- python/urllib/download_page.py
+++ python/urllib/download_page.py
@@ -7,11 +7,11 @@ import urllib2
 reload(sys)
 sys.setdefaultencoding('utf-8') 
 
-def download_page(url, proxy = None, referer = None, data = None):
+def download_page(url, proxy = None, data = None, headers = {}):
   # set http proxy
   if proxy:
-    handlers = [urllib2.ProxyHandler({'http': 'http://%s/' % proxy})]
-    opener = urllib2.build_opener(*handlers)
+    handler = urllib2.ProxyHandler({'http': 'http://%s/' % proxy})
+    opener = urllib2.build_opener(handler)
   else:
     # null_proxy_handler = urllib2.ProxyHandler({})
     # opener = urllib2.build_opener(*null_proxy_handler)
@@ -20,16 +20,22 @@ def download_page(url, proxy = None, referer = None, data = None):
   request = urllib2.Request(url, data)
   # request.get_method = lambda: 'PUT' # or 'DELETE'
 
-  # set referer
-  if referer:
-    request.add_header('Referer', referer)
-
-  # add header
-  request.add_header('User-Agent', 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36')
-  request.add_header('Accept-Language', 'en-US,en;q=0.5')
+  # set headers
+  for key, value in headers.items():
+    request.add_header(key, value)
 
   response = opener.open(request, timeout=100)
-  #response = urllib2.urlopen(request, timeout = 100).read()
+  # in python source code, urlopen call opener.open
   # redirected_url = response.geturl()
   page_buf = response.read()
   return page_buf
+
+def main():
+  url = "http://www.baidu.com"
+  header = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36'}
+
+  buf = download_page(url, None, None, header)
+  # print buf
+
+if __name__ == '__main__':
+  main()
