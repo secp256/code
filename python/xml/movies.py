8421f6fa7654e2d7782fd561800d19759208e761
zhuliting <zhuliting1986@gmail.com>
2015-12-18 11:04:07 +0800

    added

---
+++ python/xml/movies.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from xml.dom.minidom import parse
+import xml.dom.minidom
+
+DOMTree = xml.dom.minidom.parse('movies.xml')
+collection = DOMTree.documentElement
+if collection.hasAttribute('shelf'):
+  print 'Root element: %s' % collection.getAttribute('shelf')
+
+# get all movie from collection
+movies = collection.getElementsByTagName('movie')
+
+# print each movie info
+for movie in movies:
+  print '*** movie ***'
+  if movie.hasAttribute('title'):
+    print 'title: %s' % movie.getAttribute('title')
+
+  type = movie.getElementsByTagName('type')[0]
+  print 'type: %s' % type.childNodes[0].data
+
