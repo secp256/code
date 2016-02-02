94d75a99567f22c921f57450948d8caed3a511e7
zhuliting <zhuliting1986@gmail.com>
2016-02-02 16:18:22 +0800

    updated

--- cpp/boost/fun2.cpp
+++ cpp/boost/fun2.cpp
@@ -1,63 +1,30 @@
-#include <list>
 #include <iostream>
-#include <boost/lambda/lambda.hpp>
-#include <boost/bind.hpp>
+#include <vector>
 #include <boost/function.hpp>
-#include <boost/ref.hpp>
+#include <boost/bind.hpp>
+
 using namespace std;
-using namespace boost;
 
-
-int add1(int a, int b)
-{
-    return a + b;
+void print(int a) 
+{ 
+    cout << a << endl; 
 }
 
-class add2
-{
-public:
-    int operator()(int lhs, int rhs)
-    {
-        return lhs + rhs;
-    }
-};
+typedef boost::function<void (int)> SuccessPrint;
 
-class CAdd
-{
-public:
-    int add3(int a, int b) { return a + b; }
-};
+int main() 
+{ 
+    vector<SuccessPrint> printList;
 
+    SuccessPrint printOne = boost::bind(print, _1); 
+    printList.push_back(printOne); 
+    SuccessPrint printTwo = boost::bind(print, _1); 
+    printList.push_back(printTwo); 
+    SuccessPrint printThree = boost::bind(print, _1); 
+    printList.push_back(printTwo); 
 
-template<typename FUN, typename T>
-T fun(FUN function, T lhs, T rhs)
-
-    cout <<typeid(function).name() <<endl;
-    return function(lhs, rhs);
-}
-
-int add4(int a, int b, int c)
-{
-    return a+b+c;
-}
-
-int fun2(boost::function< int(int,int) > function, int a, int b )
-{
-    cout <<typeid(function).name() <<endl;
-    return function(a,b);
-}
-
-
-int main()
-{
-    boost::function< int(int,int) > padd1 = add1;
-    boost::function< int(int,int) > padd2 = add2();
-    boost::function< int(int,int) > padd3 = lambda::_1+lambda::_2;
-    boost::function< int(int,int) > padd4 = bind(add4, 0, _1, _2);
-
-    cout << fun2(padd1, 1, 1) <<endl;
-    cout << fun2(padd2, 1, 2) <<endl;
-    cout << fun2(padd3, 1, 3) <<endl;
-    cout << fun2(padd4, 1, 4) <<endl;
-    return 0;
+    // do something else
+    for (int i = 0; i < printList.size(); ++i) 
+        printList.at(i)(i);
+    return 0; 
 }
